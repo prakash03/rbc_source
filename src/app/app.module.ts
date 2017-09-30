db1c8e7cbc090e8d415392c9e4adfcdcf1309700
Rohan0401 <rohan.rajput4@gmail.com>
2017-09-30 12:24:50 -0400

    UI Change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,25 +10,13 @@ import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    UserProfileComponent,
-    TableListComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    UpgradeComponent,
+   
 
   ],
   imports: [
